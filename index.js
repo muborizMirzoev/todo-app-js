0a8ab2ca79c70764ba64dd8908cbe7106d92baaf
muborizMirzoev <muboriz10@gmail.com>
2021-08-23 04:05:54 +0700

    feat: add edit and delete button

--- index.js
+++ index.js
@@ -14,22 +14,50 @@ function addNewTodo(data = {}) {
    li.classList.add('todo__item');
    li.classList.add('new-item')
 
-   li.innerText = input.value || data.text
+   li.innerHTML = `
+      <p class="todo__text" contentEditable="false">${input.value || data.text} </p>
+      <button class="edit" type="button"><i class="fas fa-edit"></i></button>
+      <button class="delete" type="button"><i class="fas fa-trash-alt"></i></button>
+   `
    todos.append(li);
    updateLocalStorage();
    data.status ? li.classList.add('completed') : '';
 
-   li.addEventListener('click', () => {
-      li.classList.toggle('completed');
-      updateLocalStorage()
+   li.addEventListener('click', (e) => {
+      const target = e.target;
+      if (target.closest('.edit')) {
+
+         const todoText = target.closest('.edit').previousElementSibling;
+         if (todoText.contentEditable === 'false') {
+            todoText.contentEditable = true;
+            todoText.focus();
+            todoText.style.backgroundColor = '#E0E0E0';
+            todoText.style.border = '1px dotted black';
+         } else {
+            todoText.contentEditable = false;
+            todoText.style.backgroundColor = '';
+            todoText.style.border = '';
+            updateLocalStorage()
+         }
+
+      } else if (target.closest('.delete')) {
+         deleteTodo(e);
+         updateLocalStorage();
+      } else {
+         // li.classList.toggle('completed');
+         // updateLocalStorage()
+      }
    });
 
-   li.addEventListener('contextmenu', (e) => {
+   li.addEventListener('contextmenu', deleteTodo);
+
+   function deleteTodo(e) {
+      console.log(e)
       e.preventDefault()
       li.classList.add('removed-item');
       setTimeout(() => li.remove(), 500)
       updateLocalStorage();
-   });
+   }
    input.value = '';
 }
 
